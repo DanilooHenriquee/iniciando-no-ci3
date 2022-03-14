d7ced7fc60745f95bc1ba7a207a9db0b09e44b8b
Danilo Henrique <daniloohenriquee@gmail.com>
2022-03-14 20:44:56 -0300

    feita correção nos arquivos .htaccess e acrido controller Pages

---
+++ application/views/pages/home.php
@@ -0,0 +1,3 @@
+<h1>Home !</h1>
+<hr>
+<h2>Pagina padrão quando não encontrar a pagina solicitada</h2>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,3 @@
+    <em>&copy; 2022 - Desenvolvido por DanilooHenriquee</em>
+</body>
+</html>
